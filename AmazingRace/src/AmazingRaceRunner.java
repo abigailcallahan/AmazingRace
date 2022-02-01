f9ad43816274bb6c18158c950b2ed6990bd16300
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2022-02-01 09:02:15 -0700

    added intro

--- AmazingRace/src/AmazingRaceRunner.java
+++ AmazingRace/src/AmazingRaceRunner.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 public class AmazingRaceRunner
 	{
 
+		static Scanner Stringinput = new Scanner(System.in);
+		static Scanner intInput = new Scanner(System.in);
+		
+		
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				intro();
 
 			}
+		
+		
+		public static void intro()
+		{
+			System.out.println("Hello and welcome to: THE AMAZING RACE!");
+			//option to see the rules?
+			//get team 1 name
+			//get team 2 name 
+			
+		}
 
 	}
